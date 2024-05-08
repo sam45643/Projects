c0ec794147fbfe8914f90965c1491a1eaa510e84
sam45643 <122574805+sam45643@users.noreply.github.com>
2024-05-08 17:50:16 -0500

    Update script.js
    Signed-off-by: sam45643 <122574805+sam45643@users.noreply.github.com>

--- script.js
+++ script.js
@@ -12,5 +12,5 @@ const backArrow = document.querySelector('.back-arrow');
 
 backArrow.addEventListener('click', () => {
   //back button website
-  window.location.href = 'https://www.pinterest.com/search/pins/?q=unity%20logo%20gif&rs=typed';
+  window.location.href = 'https://sam45643.github.io/main-page.github.io/';
 });
