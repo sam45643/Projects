50143011a7b1976058c0b5a824fd12aac693459a
sam45643 <122574805+sam45643@users.noreply.github.com>
2024-05-12 03:36:16 -0500

    Update script.js
    Signed-off-by: sam45643 <122574805+sam45643@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const slider = document.querySelector('.slider');
 
 function activate(e) {
   const items = document.querySelectorAll('.item');
-  
-  // If the clicked element is a "Read More" button
-  if (e.target.tagName === 'BUTTON') {
-    const url = e.target.dataset.url; // Retrieve the URL from the data-url attribute of the button
-    window.location.href = url; // Navigate to the URL
-  }
-
-  // If the clicked element is a navigation button
-  if (e.target.matches('.next')) {
-    slider.append(items[0]);
-  } else if (e.target.matches('.prev')) {
-    slider.prepend(items[items.length - 1]);
-  }
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
 }
 
-document.addEventListener('click', activate, false);
+document.addEventListener('click',activate,false);
+
+const backArrow = document.querySelector('.back-arrow');
+
+backArrow.addEventListener('click', () => {
+  // Replace 'https://example.com' with the desired website URL
+  window.location.href = '';
+});
